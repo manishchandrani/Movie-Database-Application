8453ac56deaf364beff7d32c8e270d17fc71dd97
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-05 23:49:42 -0600

    Added express validator

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const movieRepo = require('../src/movierepo');
+const { body, validationResult } = require('express-validator');
 
 /* GET all movie listing */
 router.get('/', async (req, res, next) => {
@@ -8,25 +9,38 @@ router.get('/', async (req, res, next) => {
   res.render('movies',{ movies : allMovieData });
 });
 
+// Validation middleware
+const validateMovie = [
+  body('title').trim().notEmpty().withMessage('Title required').escape(),
+  body('director').trim().notEmpty().withMessage('Director required').escape(),
+  body('year').optional().trim().escape(),
+  body('notes').optional().trim().escape(),
+];
+
 /* GET Create movie Page */
 router.get('/new', (req, res, next) => {
   res.render('new',{title: 'Create a New Movie', buttonText: 'Create Movie', actionURL: 'new'});
 });
 
 /* POST movie Details from Create Movies Page */
-router.post('/new', (req, res, next) => {
+router.post('/new', validateMovie, async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('new', { title: 'Create a New Movie', buttonText: 'Create Movie', actionURL: 'new', errors: errors.array() });
+    return;
+  }
+
   const { title, director, year, notes } = req.body;
   const movie = { title: title, director: director, year: year, notes: notes};
-  movieRepo.createNewMovie(movie);
+  await movieRepo.createNewMovie(movie);
   res.redirect('/movies');
 });
 
 /* GET Single Movie View Page */
 router.get('/:id', async (req, res, next) => {
   const singleMovieData = await movieRepo.findById(req.params.id);
-  console.log("id: "+ singleMovieData);
   if(singleMovieData){
-  res.render('show', { movie: singleMovieData});
+    res.render('show', { movie: singleMovieData });
   } else {
     res.redirect('/movies');
   }
@@ -39,7 +53,13 @@ router.get('/:id/edit', async (req, res, next) => {
 });
 
 /* POST Updated movie Details from Edit Movies Page */
-router.post('/:id/edit', async (req, res, next) => {
+router.post('/:id/edit', validateMovie, async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('edit', { movie: req.body, title: 'Edit Movie', buttonText: 'Edit movie', actionURL: 'edit', errors: errors.array() });
+    return;
+  }
+
   const { title, director, year, notes } = req.body;
   const movieId = req.params.id;
   const updatedMovie = { title, director, year, notes };
@@ -63,6 +83,5 @@ router.post('/:id/delete', async (req, res, next) => {
     res.status(404).send('Movie not found');
   }
 });
-
 
 module.exports = router;
