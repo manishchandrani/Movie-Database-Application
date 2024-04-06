9d2a5736fd562290b79ad9ce97b3db4b1ef68a44
manishchandrani <manishsaikrishna.c@gmail.com>
2024-04-05 23:14:24 -0600

    added movierepo.js under src dir and added code to connect to mongodb

---
+++ src/movierepo.js
@@ -0,0 +1,17 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const Movie = require("./movie");
+
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
+
+async function connect() {
+  try {
+    await client.connect();
+    console.log("Successfully Connected to MongoDB Server");
+  } catch (err) {
+    console.error("Connection unsuccessful", err);
+  }
+}
+
+connect();
+
